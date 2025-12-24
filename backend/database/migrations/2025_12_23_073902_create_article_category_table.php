9710a328192503298273a26754440fcf6e6fb21d
Faoussane <yfaoussane@gmail.com>
2025-12-24 08:07:24 +0100

    Add foreign keys to article_category table

--- backend/database/migrations/2025_12_23_073902_create_article_category_table.php
+++ backend/database/migrations/2025_12_23_073902_create_article_category_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+#Table pivot///
 return new class extends Migration
 {
     /**
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('article_category', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('article_id')->constrained('news_articles')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('article_categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
